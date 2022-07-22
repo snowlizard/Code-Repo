6a2bf0ad212f492e4d189247aec3b349f9465fc5
Julian <illegal_burrito@live.com>
2022-07-22 10:58:57 -0700

    event queue not processing correctly

--- codeSnippets/DailyChangeRequestsPlannedToday.js
+++ codeSnippets/DailyChangeRequestsPlannedToday.js
@@ -5,5 +5,15 @@ changeRequests.addEncodedQuery('start_dateONToday@javascript:gs.beginningOfToday
 changeRequests.query();
 
 while (changeRequests.next()) {
-	gs.eventQueue('scheduled_job.change_request', changeRequests, changeRequests.getValue('start_date'), changeRequests.getValue('sys_id'));
+    var time = new GlideTime();
+    time.setValue("01:00:00");
+
+    var start_date = new GlideDateTime();
+    start_date.setValue(changeRequests.start_date);
+    start_date.subtract(time);
+
+
+    gs.eventQueueScheduled('scheduled_job.change_request', changeRequests,
+        changeRequests.getValue('number'), changeRequests.getValue('sys_id'),
+        start_date.getDisplayValue());
 }
--- codeSnippets/changeRequestPlannedStartNotifcation.js
+++ codeSnippets/changeRequestPlannedStartNotifcation.js
@@ -1,17 +1,3 @@
 // Script action
 
-var plannedStart = new GlideDateTime();
-plannedStart.setValue(current.start_date);
-var hourAgo = new GlideTime();
-hourAgo.setValue("01:00:00");
-plannedStart.subtract(hourAgo);
-
-// all datetime data is stored in GMT in servicenow must convert to userlocal time for correct scheduling
-var start_date = new GlideDateTime();
-start_date.setValue(current.getValue('start_date'));
-
-var scheduledTime = plannedStart.getLocalDate() + ' ' + plannedStart.getUserFormattedLocalTime();
-var gdt = new GlideDateTime();
-gdt.setValue(scheduledTime);
-
 gs.eventQueueScheduled('scheduled_job.notification', current, current.assigned_to, current.number, gdt);
